92d459e9414af9731fb319a802f23e9ec8f7ebbe
Hunt, Neha <neha.agrawal@philips.com>
2024-03-19 12:52:34 +0530

    create header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,15 @@ import Signin from './pages/Signin';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
+import Header from './components/Header';
 
 
 export default function App() {
-  return <BrowserRouter>
+  return(
+   <BrowserRouter>
+  <Header />
   <Routes>
+    
     <Route path="/" element={<Home />} />
     <Route path="/sign-in" element={<Signin/>} />
     <Route path="/sign-up" element={<SignUp />} />
@@ -16,7 +20,8 @@ export default function App() {
     <Route path="/profile" element={<Profile />} />
   
   </Routes>
-  </BrowserRouter>;
+  </BrowserRouter>
+  );
     
   
 }
